b207f72bee8c0481d66dde127970dfc4a8a5dd1a
I750911 <till.kurek@sap.com>
2024-11-06 17:16:31 +0100

    Initial commit

---
+++ src/main/java/com/example/vertex_ai/spec/test/VertexAiSpecTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.vertex_ai.spec.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VertexAiSpecTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VertexAiSpecTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vertex_ai/spec/test/VertexAiSpecTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vertex_ai.spec.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VertexAiSpecTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
